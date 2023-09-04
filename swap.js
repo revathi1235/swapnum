f02d93bead2e52dc7024e3fa4437aded44c5ceea
revathi1235 <revathivenugopal10@gmail.com>
2023-09-04 12:14:24 +0530

    first commit

---
+++ swap.js
@@ -0,0 +1,12 @@
+let a,b,c;
+function swap(a,b)
+{
+   console.log("value of a is "+a);
+   console.log("value of b is "+b);
+    c=a;
+    a=b;
+    b=c;
+ console.log("value of a is "+a);
+  console.log("value of b is "+b);
+}
+swap(1,2);
